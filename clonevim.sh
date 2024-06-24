0c996d7c2e79637dcb75f5361dba31d435d5ad4d
LiNk-NY <marcel.ramos@roswellpark.org>
2024-06-24 11:53:03 -0400

    update variables

--- clonevim.sh
+++ clonevim.sh
@@ -4,26 +4,29 @@
 
 pkgname=$1
 
-BIOC="$HOME/reviews"
+REVIEWS="$HOME/reviews"
+BIOC="devel"
 
-PKGLOC=$BIOC/$pkgname
+PKGLOC=$REVIEWS/$pkgname
+
+RVER="r-4-4"
 
 if [ -z "${pkgname// }" ]; then
     echo "Enter a package folder name"
     exit 1
 elif [ ! -d $PKGLOC ]; then
-    cd $BIOC
+    cd $REVIEWS
     git clone git@git.bioconductor.org:packages/${pkgname}.git
 fi
 
 cd $PKGLOC
 
-biocfile="$BIOC/BiocReviews/packages/${pkgname}_review.txt"
+biocfile="$REVIEWS/BiocReviews/packages/${pkgname}_review.txt"
 
 echo $biocfile
 if [ ! -e "$biocfile" ] ; then
     touch $biocfile
 fi 
 
-R_LIBS_USER=$HOME/R/bioc-devel RLOC=$HOME/src/svn/r-devel/R/bin RCOMP=$HOME/.cache/Nvim-Rd /usr/local/bin/vim -O $biocfile DESCRIPTION NAMESPACE vignettes/*.Rmd R/*R
+R_LIBS_USER=$HOME/R/bioc-${BIOC} RLOC=$HOME/src/svn/${RVER}/R/bin RCOMP=$HOME/.cache/Nvim-Rd /usr/local/bin/vim -O $biocfile DESCRIPTION NAMESPACE vignettes/*.Rmd R/*R
 
